bf55133895467eccf32f28ceb4dad9e44683d942
kishan <kishansharma6377@gmail.com>
2021-10-04 11:11:43 +0530

    initial commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,12 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+import 'bootstrap/dist/js/bootstrap'
+import 'bootstrap/dist/css/bootstrap'
+require("stylesheets/application.scss")
+
+
+
+// Rails.start()
+// Turbolinks.start()
+// ActiveStorage.start()
